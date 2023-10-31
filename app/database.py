48c4327404b99a2a976a301b29cc397b83dcf220
exoutia <jhav365@gmail.com>
2023-10-31 22:03:02 +0530

    migrating from 1.4 sqlalchemy syntx to 2.0 sqlalchemy syntax

--- app/database.py
+++ app/database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
 from .config import Settings
 
@@ -17,7 +16,8 @@ engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+class Base(DeclarativeBase):
+    pass
 
 
 def get_db():
--- app/models.py
+++ app/models.py
@@ -1,14 +1,29 @@
-from sqlalchemy import Column, Integer, String
+from typing import Annotated
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .database import Base
+
+intpk = Annotated[int, mapped_column(primary_key=True, index=True)]
+strnull = Annotated[str, mapped_column(String, nullable=True)]
 
 
 class Problem(Base):
     __tablename__ = "problems"
 
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=False)
-    description = Column(String)
-    link = Column(String)
-    solution_link = Column(String)
-    difficulty = Column(String)
+    id: Mapped[intpk]
+    title: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[strnull]
+    link: Mapped[strnull]
+    solution_link: Mapped[strnull]
+    difficulty: Mapped[strnull]
+    category: Mapped[strnull]
+
+
+class Category(Base):
+    __tablename__ = "categories"
+
+    id: Mapped[intpk]
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    description: Mapped[strnull]
