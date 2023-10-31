293a2863009d2add3ad2f6274d149aef2fafe8f1
exoutia <jhav365@gmail.com>
2023-10-31 14:36:54 +0530

    adding crud operations into the api for creating problems and everything.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException, status
 from sqlalchemy.orm import Session
 
 from . import database, models, schemas
@@ -10,16 +10,11 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "This is questions"}
+    return {"message": "This is problems"}
 
 
-@app.get("/questions", response_model=List[schemas.Problem])
-def get_all_questions(db: Session = Depends(database.get_db)):
-    return db.query(models.Problem).all()
-
-
-@app.post("/questions", response_model=schemas.Problem)
-def create_question(
+@app.post("/problems", response_model=schemas.Problem)
+def create_problem(
     problem: schemas.ProblemCreate, db: Session = Depends(database.get_db)
 ):
     db_problem = models.Problem(
@@ -33,3 +28,51 @@ def create_question(
     db.commit()
     db.refresh(db_problem)
     return db_problem
+
+
+@app.get("/problems", response_model=List[schemas.Problem])
+def get_all_problems(
+    db: Session = Depends(database.get_db), limit: int = 10, skip: int = 0
+):
+    query = db.query(models.Problem)
+    problems = query.limit(limit).offset(skip).all()
+    if not problems:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="No Problem Found"
+        )
+    return problems
+
+
+@app.put(
+    "/problems/{problem_id}",
+    response_model=schemas.ProblemCreate,
+    status_code=status.HTTP_202_ACCEPTED,
+)
+def update_problem(
+    problem_id: int,
+    update_problem: schemas.ProblemCreate,
+    db: Session = Depends(database.get_db),
+):
+    query = db.query(models.Problem).filter(models.Problem.id == problem_id)
+    problem = query.first()
+    if problem is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"problem with {problem_id} is not found.",
+        )
+    query.update(update_problem.dict(), synchronize_session=False)
+    db.commit()
+    return update_problem
+
+
+@app.delete("/problems/{problem_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_problem(problem_id: int, db: Session = Depends(database.get_db)):
+    query = db.query(models.Problem).filter(models.Problem.id == problem_id)
+    if query.first is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"The problem with {problem_id} id is not found",
+        )
+    query.delete(synchronize_session=False)
+    db.commit()
+    return
--- app/schemas.py
+++ app/schemas.py
@@ -9,10 +9,21 @@ class Difficulty(str, Enum):
     HARD = "hard"
 
 
-class Problem(BaseModel):
+class BaseProblem(BaseModel):
+    title: str
+
+
+class Problem(BaseProblem):
     id: int
     title: str
     description: str | None
     difficulty: Difficulty | None
     link: HttpUrl | None
     solution_link: HttpUrl | None
+
+
+class ProblemCreate(BaseProblem):
+    description: str | None = None
+    difficulty: Difficulty | None = None
+    link: HttpUrl | None = None
+    solution_link: HttpUrl | None = None
