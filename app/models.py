b6d28e22394b131c510c79a882a1cf46b9924081
exoutia <jhav365@gmail.com>
2023-11-03 22:16:08 +0530

    migrating to modern sqlalchemy with the support for typing.

--- app/models.py
+++ app/models.py
@@ -7,13 +7,13 @@ from .database import Base
 
 intpk = Annotated[int, mapped_column(primary_key=True, index=True)]
 strnull = Annotated[str, mapped_column(String, nullable=True)]
-
+required_str = Annotated[str, mapped_column(String, nullable=False)]
 
 class Problem(Base):
     __tablename__ = "problems"
 
     id: Mapped[intpk]
-    title: Mapped[str] = mapped_column(String, nullable=False)
+    title: Mapped[required_str]
     description: Mapped[strnull]
     link: Mapped[strnull]
     solution_link: Mapped[strnull]
@@ -25,5 +25,5 @@ class Category(Base):
     __tablename__ = "categories"
 
     id: Mapped[intpk]
-    name: Mapped[str] = mapped_column(String, nullable=False)
+    name: Mapped[required_str]
     description: Mapped[strnull]
